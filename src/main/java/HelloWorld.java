503379aacb80eece4b1496399cb02543fe7c6e56
hnz678 <zhenghualin2008@hotmail.com>
2021-06-20 12:01:21 -0400

    main changes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println(StringUtils.capitalize("hello world"));
+        System.out.println("master changes");
+        System.out.println("test changes");
     }
 }
