a0b6e41725f0a569f751bd15733e27df2a7212e8
hnz678 <zhenghualin2008@hotmail.com>
2021-06-20 12:14:08 -0400

    master changes 3

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,10 +4,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println(StringUtils.capitalize("hello world"));
-        System.out.println("master2 changes");
+        System.out.println("master changes 2");
         System.out.println("master changes");
         System.out.println("test changes");
         System.out.println("test2 changes");
+        System.out.println("master changes 3");
 
     }
 }
